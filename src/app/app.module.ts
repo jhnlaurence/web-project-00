80924ccb8d38f2db71a10055231daf921223f28d
jhnlaurence <lebarioslaurence@gmail.com>
2023-08-02 15:02:15 +0800

    [WEB-006] add: Footer addition to the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/home/home.component';
 import { NavigationBarComponent } from './core/components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
